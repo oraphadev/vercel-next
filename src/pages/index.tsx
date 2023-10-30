456ada595e23eb3a20063eed8ebea41401eae656
Raphael Lima <oraphadev@gmail.com>
2023-10-29 21:42:04 -0300

    feat(master): ✨ first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+import { Inter } from 'next/font/google'
+import { useState } from 'react'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Head>
+        <title>React + Next.js + Vercel</title>
+        <meta name="description" content="Projeto em React com Next.js e hospedagem na Vercel" />
+      </Head>
+
+      <main className={`p-24 text-center ${inter.className}`}>
+        <h1 className="text-3xl font-bold mb-4">{count}</h1>
+
+        <Button
+          variant="secondary"
+          className="mb-12"
+          onClick={() => setCount(count + 1)}
+        >
+          Incrementar contador
+          <Plus className="w-4 h-4 ml-2" />
+        </Button>
+
+        <h2 className="text-3xl font-bold mb-4">
+          @oraphadev
+        </h2>
+
+        <p className="text-sm text-muted-foreground">
+          React - Next.js - Vercel
+        </p>
+      </main>
+    </>
+  )
+}
